9f410b45217d8f46ab636dd5170ff44e803208d4
RSsunny <sunnysharif154@gmail.com>
2023-09-01 04:08:34 +0600

    drawing empty container added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const getCategory=async()=>{
+const getCategory=async(short)=>{
     const res=await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const data=await res.json()
     pushcategory(data.data)
@@ -9,22 +9,27 @@ const pushcategory=(data)=>{
         const categoryList=document.createElement('div')
         categoryList.classList=``
         categoryList.innerHTML=`
-        <button onclick="cardContainer(${data.category_id})" class="px-4 py-2 bg-gray-300 text-base font-medium hover:bg-scondary-color focus:text-white focus:bg-scondary-color  list-none rounded-md cursor-pointer">${data.category}</button>
+        <button onclick="cardContainer(${data.category_id})" class="px-1 md:px-4 py-1 md:py-2 md:bg-gray-300 text-base font-medium md:hover:bg-scondary-color focus:border-b focus:border-red-600 focus:text-red-500  md:focus:text-white md:focus:bg-scondary-color  list-none md:rounded-md cursor-pointer">${data.category}</button>
         `
         categoryContainer.appendChild(categoryList)
     })
 }
-const cardContainer=async(id)=>{
+const cardContainer=async(id,short)=>{
     const res=await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data=await res.json()
-    pushCard(data.data)
+    pushCard(data.data,short)
 }
-const pushCard=(data)=>{
+const pushCard=(data,short)=>{
+  if(short==='short'){
+    console.log('dfhdhf');
+  }
     const cardContainer=document.getElementById('card-container')
+    const emptyContainer=document.getElementById('empty-container')
     cardContainer.textContent=' '
+    emptyContainer.textContent=' '
+    if(data.length>0){  
     data.forEach(data=>{
         console.log(data); 
-
         const scondTotal=data.others.posted_date
         const hours=Math.floor(scondTotal/3600)
         const minutes=Math.floor((scondTotal%3600)/60)
@@ -33,7 +38,7 @@ const pushCard=(data)=>{
         <div class="card rounded-none  bg-gray-200 text-black">
         <figure class="h-[200px] relative">
         <img class="hover:scale-125 hover:overflow-hidden duration-300 h-full w-full" src=${data.thumbnail} /> 
-        <div class="bg-black bg-opacity-70  text-white px-2 py-1 rounded-sm absolute right-2 bottom-1" >${scondTotal>0? `${hours} hrs ${minutes} min ago`: ' '} </div> 
+        <div class="bg-black bg-opacity-70  text-white px-2 py-1 rounded-sm absolute right-2 bottom-1" >${scondTotal>0 && scondTotal<16726560? `${hours} hrs ${minutes} min ago`: ' '} </div> 
       </figure>
         <div class=" ml-4 my-5 ">    
           <div class="card-footer flex justify-between mt-4">
@@ -62,8 +67,27 @@ const pushCard=(data)=>{
         `
         cardContainer.appendChild(div)
     })
-    
+    }
+    else{
+      const div=document.createElement('div')
+      div.classList=`flex justify-center items-center max-w-[500] mx-auto `
+        div.innerHTML=`
+        <div class="card  ">
+        <figure class="px-10 pt-10">
+       <img src="./image/fi_5301987.svg" alt="Shoes" class="rounded-xl" />
+       </figure>
+        <div class="card-body items-center text-center">
+       <h2 class="card-title text-3xl font-bold">Oops!! Sorry, There is no content here</h2>
+       </div>
+      </div>
+        `
+        emptyContainer.appendChild(div)
+    } 
 }
+const shortVew=()=>{
+  cardContainer('short')
 
+  
+}
 getCategory()
 cardContainer('1000')
